a6a6171d8c4021a800d13e78ccac4786cd292386
Maruu <mari.calvo1607@gmail.com>
2026-02-17 13:24:16 -0500

    ya puedo poner numeros yey

--- My project (1)/Assets/Calculadora/Scripts/Calculadora.cs
+++ My project (1)/Assets/Calculadora/Scripts/Calculadora.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,11 +14,149 @@ public class Calculadora : MonoBehaviour
     public void iAddition()
     {
         num1 = float.Parse(resultText.text);
+        operation = ("+");
+        resultText.text = "";
     }
 
-   
-    void Update()
+    public void iSubstration()
     {
-        
+        num1 = float.Parse(resultText.text);
+        operation = ("-");
+        resultText.text = "";
+    }
+
+    public void iDivision()
+    {
+        num1 = float.Parse(resultText.text);
+        operation = ("/");
+        resultText.text = "";
+    }
+
+    public void iMultiplication()
+    {
+        num1 = float.Parse(resultText.text);
+        operation = ("*");
+        resultText.text = "";
+    }
+
+    public void btndigit7()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "7";
+        }
+        else
+        {
+            resultText.text = resultText.text + "7";
+        }
+    }
+
+    public void btndigit8()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "8";
+        }
+        else
+        {
+            resultText.text = resultText.text + "8";
+        }
+    }
+
+    public void btndigit9()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "9";
+        }
+        else
+        {
+            resultText.text = resultText.text + "9";
+        }
+    }
+
+    public void btndigit4()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "4";
+        }
+        else
+        {
+            resultText.text = resultText.text + "4";
+        }
+    }
+
+    public void btndigit5()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "5";
+        }
+        else
+        {
+            resultText.text = resultText.text + "5";
+        }
+    }
+
+    public void btndigit6()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "6";
+        }
+        else
+        {
+            resultText.text = resultText.text + "6";
+        }
+    }
+
+    public void btndigit1()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "1";
+        }
+        else
+        {
+            resultText.text = resultText.text + "1";
+        }
+    }
+
+    public void btndigit2()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "2";
+        }
+        else
+        {
+            resultText.text = resultText.text + "2";
+        }
+    }
+
+    public void btndigit3()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "3";
+        }
+        else
+        {
+            resultText.text = resultText.text + "3";
+        }
+    }
+
+    public void btndigit0()
+    {
+        if (resultText.text == Convert.ToString("0"))
+        {
+            resultText.text = "0";
+        }
+        else
+        {
+            resultText.text = resultText.text + "0";
+        }
     }
 }
+
